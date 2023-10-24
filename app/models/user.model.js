b466941814c35d037c4fceb5cfbc54d56c4f1d0d
Andrew Moyer <andrew.moyer@eagles.oc.edu>
2023-10-24 15:44:41 -0500

    Fixed userModel foreign key incongruence

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("user", {
-    userID: {
+    id: {
       type: Sequelize.INTEGER,
       autoIncrement: true,
       primaryKey: true,
